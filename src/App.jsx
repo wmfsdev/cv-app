1effdd86d09c53622c3e387507f0ffb754bf27bc
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-11-07 00:28:22 +0000

    implement basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { cvData } from './components/data'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleClick(e) {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    console.log(data.get("name"))
+    console.log(data.get("email"))
+    console.log(data.get("tel"))
+  }
+
+  function handleChange(e) {
+
+  }
+  
+
+  function Form() {
+  const [cv, setCv] = useState(cvData)
+    return (
+      <>
+      <form onSubmit={handleClick}>
+        <h2>BASIC INFO</h2>
+        <Fieldset>
+          <Label>
+            <Input></Input>
+          </Label>
+        </Fieldset>
+
+        <button>SEND</button>
+        <button>EDIT</button>
+      </form>
+      </>
+    )
+  }  
+  
+  function Label({children}) {
+    const labels = ["Name: ", "Email: ", "Tel: ", "Test: "]
+    const listLabels = labels.map((label, index) => {
+      return <label htmlFor='tets' key={index}>{label}
+        {children}
+      </label>
+    })
+    return listLabels
+  }
+
+  function Input() {  // {person}
+  //  const inputList = cv.
+    return (
+      <input
+        id="hey"
+        name="hey"
+        value="test" // {person.basicInfo.name}
+        onChange={handleChange}
+      />
+    )
+  }
+
+  function Fieldset({children}) {
+    const categories = ["BASIC INFO", "EDUCATION", "WORK"]
+    const listCategories = categories.map((category, index) => {
+      return (
+        <>
+        <fieldset>
+          <legend>{category}</legend>
+          {children}
+        </fieldset>
+        </>
+      )
+    })
+    return listCategories
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Form></Form>
   )
+
 }
 
 export default App
---
+++ src/components/data.jsx
@@ -0,0 +1,21 @@
+
+export const cvData = 
+    {
+      basicInfo: {
+        name: 'john',
+        email: 'doe',
+        tel: 1234,
+      },
+      
+    }
+  
+  {/* <label htmlFor="name">Name:{' '}
+          <Input 
+            
+            person={cv}
+          />
+        </label> */}
+
+
+
+   //     {label}
